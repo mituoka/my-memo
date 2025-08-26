375e55a02caf4e4fe1026498a71cbd4d22067300
mituoka <mitutumi0210@gmail.com>
2025-08-27 05:56:11 +0900

    絵文字アイコンの更新

--- src/pages/MemoDetail.tsx
+++ src/pages/MemoDetail.tsx
@@ -6,15 +6,38 @@ import { parseMarkdown } from '../utils/markdownUtils';
 import DeleteConfirmModal from '../components/modals/DeleteConfirmModal';
 import ImageViewerModal from '../components/modals/ImageViewerModal';
 
-// メモタイプのラベルを取得するヘルパー関数
-function getMemoTypeLabel(type?: string): string {
+// メモタイプのラベルとアイコンを取得するヘルパー関数
+function getMemoTypeInfo(type?: string): { label: string; icon: JSX.Element } {
+  const iconProps = { width: "14", height: "14", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24", strokeWidth: "1.5" };
+  
   switch (type) {
     case 'note':
-      return '📝 ノート';
+      return {
+        label: 'ノート',
+        icon: (
+          <svg {...iconProps}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+          </svg>
+        )
+      };
     case 'wiki':
-      return '📚 Wiki';
+      return {
+        label: 'Wiki',
+        icon: (
+          <svg {...iconProps}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+          </svg>
+        )
+      };
     default:
-      return '📄 メモ';
+      return {
+        label: 'メモ',
+        icon: (
+          <svg {...iconProps}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+          </svg>
+        )
+      };
   }
 }
 
